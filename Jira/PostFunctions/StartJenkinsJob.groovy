f4e2c19da6f3e1e3d0c77d27438db97051bbd9d7
jherio <robinson.jhe@gmail.com>
2024-08-25 23:43:04 +0100

    Refactoring code and instructions on use

--- Jira/PostFunctions/StartJenkinsJob.groovy
+++ Jira/PostFunctions/StartJenkinsJob.groovy
@@ -1,53 +1,85 @@
+/*
+ * StartJenkinsJob.groovy
+ * 
+ * This script triggers a Jenkins job based on Jira issue custom field values.
+ * 
+ * Instructions:
+ * 1. Replace the placeholders in the 'jenkinsJobFields' list with your actual custom field IDs.
+ * 2. Set up the following application properties in Jira:
+ *    - JENKINS_URL_PROPERTY_KEY: The URL of your Jenkins instance
+ *    - JENKINS_JOB_PATH_PROPERTY_KEY: The path to the Jenkins job you want to trigger
+ *    - JENKINS_USERNAME_PROPERTY_KEY: The username for Jenkins authentication
+ *    - JENKINS_API_TOKEN_PROPERTY_KEY: The API token for Jenkins authentication
+ * 3. Add this script as a post-function in your Jira workflow.
+ * 
+ * Note: Ensure that the user running this script has the necessary permissions
+ * to read the application properties and access the custom fields.
+ */
+
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.MutableIssue
 import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.Method
-import javax.ws.rs.core.Response
 import groovyx.net.http.ContentType
-import com.atlassian.jira.issue.IssueManager
-import com.atlassian.jira.issue.MutableIssue
-import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.issue.util.DefaultIssueChangeHolder
-import com.atlassian.jira.issue.ModifiedValue
-import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.config.properties.ApplicationProperties
 
+// Get the current issue
+def issue = issue as MutableIssue
 
-IssueManager im = ComponentAccessor.getIssueManager()
-MutableIssue issue = im.getIssueObject("JSD-1")
+if (issue) {
+    log.info("Processing issue ${issue.key}, retrieving custom fields...")
 
-if(issue){
-log.info("Found Issue, retrieving custom fields..")
+    def customFieldManager = ComponentAccessor.getCustomFieldManager()
+    def jenkinsJobFields = [
+        "CUSTOM_FIELD_ID_1",
+        "CUSTOM_FIELD_ID_2",
+        "CUSTOM_FIELD_ID_3",
+        "CUSTOM_FIELD_ID_4",
+        "CUSTOM_FIELD_ID_5",
+        "CUSTOM_FIELD_ID_6"
+    ]
 
-def jenkins_job_1_cf_val = issue.getCustomFieldValue(ComponentAccessor.getCustomFieldManager().getCustomFieldObject("customfield_11122"))
+    // Retrieve custom field values
+    def jenkinsJobValues = jenkinsJobFields.collectEntries { fieldId ->
+        def customField = customFieldManager.getCustomFieldObject(fieldId)
+        [(customField.name): issue.getCustomFieldValue(customField)]
+    }
 
-def jenkins_job_2_cf_val = issue.getCustomFieldValue(ComponentAccessor.getCustomFieldManager().getCustomFieldObject("customfield_15202"))
+    // Prepare Jenkins job parameters
+    def data = jenkinsJobValues.findAll { it.value != null }
 
-def jenkins_job_3_cf_val = issue.getCustomFieldValue(ComponentAccessor.getCustomFieldManager().getCustomFieldObject("customfield_15203"))
+    // Retrieve Jenkins configuration from application properties
+    def applicationProperties = ComponentAccessor.getComponent(ApplicationProperties)
+    def jenkinsUrl = applicationProperties.getString("JENKINS_URL_PROPERTY_KEY")
+    def jenkinsJobPath = applicationProperties.getString("JENKINS_JOB_PATH_PROPERTY_KEY")
+    def jenkinsUsername = applicationProperties.getString("JENKINS_USERNAME_PROPERTY_KEY")
+    def jenkinsApiToken = applicationProperties.getString("JENKINS_API_TOKEN_PROPERTY_KEY")
 
-def jenkins_job_4_cf_val = issue.getCustomFieldValue(ComponentAccessor.getCustomFieldManager().getCustomFieldObject("customfield_15204"))
+    if (!jenkinsUrl || !jenkinsJobPath || !jenkinsUsername || !jenkinsApiToken) {
+        log.error("Jenkins configuration is incomplete. Please check application properties.")
+        return
+    }
 
-def jenkins_job_4_cf_val = issue.getCustomFieldValue(ComponentAccessor.getCustomFieldManager().getCustomFieldObject("customfield_14302"))
+    def httpBuilder = new HTTPBuilder("${jenkinsUrl}/job/${jenkinsJobPath}/buildWithParameters")
 
-def jenkins_job_5_cf_val_list = issue.getCustomFieldValue(ComponentAccessor.getCustomFieldManager().getCustomFieldObject("customfield_13709"))
+    try {
+        String userPassBase64 = "${jenkinsUsername}:${jenkinsApiToken}".bytes.encodeBase64().toString()
 
-//Store your jenkin parameter here as a map
-def data = [JJ1: "$jenkins_job_1_cf_val", JJ2: "$jenkins_job_2_cf_val", JJ3: "$jenkins_job_2_cf_val", JJ3: "$jenkins_job_3_cf_val", JJ4: "$jenkins_job_4_cf_val", JJ5: "$jenkins_job_5_cf_val"]
+        httpBuilder.request(Method.POST, ContentType.JSON) { req ->
+            headers."Authorization" = "Basic ${userPassBase64}"
+            uri.query = data
 
-def httpBuilder = new HTTPBuilder("http://<JenkinsURL>/job/<JobPath>/buildWithParameters")
+            response.success = { resp, json ->
+                log.info("Jenkins job triggered successfully for issue ${issue.key}. Status: ${resp.status}")
+            }
 
-def username='JenkinsUser'
-def password='JenkinsUserToken'
-
-String userPassBase64 = "$username:$password".toString().bytes.encodeBase64()
-
-httpBuilder.setHeaders(["Authorization": "Basic $userPassBase64"])
-httpBuilder.request(Method.POST, ContentType.JSON) {
-uri.query = data
-response.success = {resp, json ->
-log.warn "request succeed with status ${resp.status}"
-//log.warn "request succeed with status ${resp.status}, response body was [${resp.entity.content.text}]"
-}
-response.failure = { resp, json ->
-log.warn "request failed with status ${resp.status}"
-//log.warn "request failed with status ${resp.status} response body was [${resp.entity.content.text}]"
-}
-}
+            response.failure = { resp, json ->
+                log.error("Failed to trigger Jenkins job for issue ${issue.key}. Status: ${resp.status}, Response: ${json}")
+            }
+        }
+    } catch (Exception e) {
+        log.error("Error occurred while triggering Jenkins job for issue ${issue.key}", e)
+    }
+} else {
+    log.warn("No issue found in the current context")
 }
